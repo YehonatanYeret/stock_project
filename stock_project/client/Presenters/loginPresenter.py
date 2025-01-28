6311fefa9eb4644a23a66456ef3eb1862ebc1175
maor-noy <maornoy1310@gmail.com>
2025-01-28 17:52:33 +0200

    i forgoten a file

--- stock_project/client/Presenters/loginPresenter.py
+++ stock_project/client/Presenters/loginPresenter.py
@@ -12,7 +12,7 @@ class LoginPresenter:
         password = self.view.get_password()
         #בדיקת האימות לא צריכה להיות פה !!!
         if self.model.authenticate(username, password):
-            self.view.show_message("Login successful!")
+            self.view.show_signin_message("Login successful!")
             self.controller.switch_to_dashboard()
         else:
             self.view.show_signin_message("Invalid username or password. Please try again.")
